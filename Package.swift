e507553a9666f80695c20b0f75553dff78a5d84f
Hai Feng Kao <>
2023-04-27 20:17:27 +0800

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+	name: "SwiftDrawer",
+	platforms: [
+		.iOS(.v11),
+                .macOS(.v11)
+	],
+	products: [
+		.library(
+			name: "SwiftDrawer",
+			targets: ["SwiftDrawer"]
+		)
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "SwiftDrawer",
+			dependencies: []
+		)
+
+        ]
+)
