e1f5c981af140144cf71bd2ea86891c868b38756
Hai Feng Kao <>
2023-04-28 16:45:47 +0800

    code format

--- Package.swift
+++ Package.swift
@@ -2,23 +2,22 @@
 import PackageDescription
 
 let package = Package(
-	name: "SwiftDrawer",
-	platforms: [
-		.iOS(.v11),
-                .macOS(.v11)
-	],
-	products: [
-		.library(
-			name: "SwiftDrawer",
-			targets: ["SwiftDrawer"]
-		)
-	],
-	dependencies: [],
-	targets: [
-		.target(
-			name: "SwiftDrawer",
-			dependencies: []
-		)
-
-        ]
+    name: "SwiftDrawer",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "SwiftDrawer",
+            targets: ["SwiftDrawer"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftDrawer",
+            dependencies: []
+        ),
+    ]
 )
